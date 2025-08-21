23029c14be50bcba0272510ff2f3944856090cd7
zhoukaidong <zkdhzr@gmail.com>
2025-08-21 17:28:42 +0800

    fix：优化

--- contoller/clash.js
+++ contoller/clash.js
@@ -2,9 +2,13 @@ const Router = require('@koa/router');
 const router = new Router();
 const clash = require('../service/clash');
 const query = async ctx => {
-  const { type = 'Clash' } = ctx.query;
-  ctx.set('Content-disposition', `attachment; filename=${type}_${Date.now()}.yaml`);
-  ctx.set('Content-type', 'application/x-yaml');
+  const { type = 'Clash', view = '0' } = ctx.query;
+  if (view === '1') {
+    ctx.set('Content-Type', 'text/plain; charset=utf-8');
+  } else {
+    ctx.set('Content-disposition', `attachment; filename=${type}_${Date.now()}.yaml`);
+    ctx.set('Content-type', 'application/x-yaml');
+  }
   ctx.set('Subscription-Userinfo', 'upload=5209967462; download=801157997; total=161293008896; expire=1777375476');
   ctx.body = await clash.createConfig(type);
 };
